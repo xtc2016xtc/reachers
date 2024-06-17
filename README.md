b8542d033b0c592162ad690e2ee8f283ef696b7b
xtc2016xtc <728611827@qq.com>
2024-06-17 12:45:36 +0800

    更新第三方库@gsap/react和gsap，并完成第三方库的测试

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # reachers
 <h1>gitub源码地址
-<a href="https://github.com/xtc2016xtc/reachers.git">点击这里，前往</a>
+  <a href="https://github.com/xtc2016xtc/reachers.git">点击这里，前往</a>
 </h1>
+<div></div>
 
 
--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,6 +1,17 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 const GsapFrom = () => {
   // TODO: Implement the gsap.from() method
-
+  useGSAP(()=>{
+    gsap.to('#green-box',{
+      x:250,
+      repeat:-1,
+      yoyo:true,
+      rotation:360,
+      duration:2,
+      ease:'elastic'
+    })
+  },[])
   return (
     <main>
       <h1>GsapFrom</h1>
