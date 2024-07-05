2a63a18a4e755448781a12ce11570e93a381463a
xtc2016xtc <728611827@qq.com>
2024-07-05 17:36:42 +0800

    修复华为浏览器控件显示问题

--- src/components/Macviews/GetStarted/index.jsx
+++ src/components/Macviews/GetStarted/index.jsx
@@ -1,8 +1,10 @@
 import styles from "../style"
 import { arrowUp } from "../images"
 const GetStarted = () => (
-  <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient`}>
-
+  <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+    <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%]`}>
+      
+    </div>
   </div>
 )
 
