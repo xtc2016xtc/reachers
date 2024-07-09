01306e645ee3a240d4189af7efd143be0e1dad36
xtc2016xtc <728611827@qq.com>
2024-07-09 17:01:44 +0800

    bug8行第三个字符

--- src/components/Macviews/Stats/index.jsx
+++ src/components/Macviews/Stats/index.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import { stats } from "../../../constants/Mac"
+import styles from "../style"
 
 const Stats = () => {
   return (
-    <div>
-      Stats
-    </div>
+    <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+        {stats.map((stat) => (
+            <div key={stat.id} className="">
+              <h4>
+                {stat.value}
+              </h4>
+              <p>
+                {stat.title}
+              </p>
+            </div>
+        ))}
+    </section>
   )
 }
 
--- src/components/lers/Lers.jsx
+++ src/components/lers/Lers.jsx
@@ -3,7 +3,7 @@
 const Lers = () => {
   return (
     <div>
-     1.06版本
+     1.09.41版本
     </div>
   )
 }
