512fc5864ef0c4ca921a94542f0213b66ac922d6
xtc2016xtc <728611827@qq.com>
2024-07-11 16:25:35 +0800

    跳转导航

--- src/components/Macviews/Business/index.jsx
+++ src/components/Macviews/Business/index.jsx
@@ -1,9 +1,43 @@
-import styles from '../style'
+import styles,{ layout } from '../style'
+import { features } from '../../../constants/Mac'
+import Button from '../components/Button'
+
+const FeatureCard = ({icon,title,content,index}) => (
+  <div 
+  className={`flex flex-row p-6 rounded-[20px] ${index !== features.length -1 ? "mb-6" : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="icon" className='w-[50%] h-[50%] object-contain'/>
+    </div>
+    <div className='flex-1 flex flex-col ml-3'>
+      <h4 className='font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1'>
+        {title}
+      </h4>
+      <p className='font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1'>
+        {content}
+      </p>
+    </div>
+  </div>
+)
 
 const Business = () => {
   return (
-    <section className={`${styles.flexCenter} flex flex-1 ss:hidden`}>
+    <section id='features' className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2
+          className={styles.heading2}
+        >为了更好地应对<br  className='sm:block hidden'/>AI技术的发展</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+            我们致力于为AI开发者提供最全面、最专业的工具和服务，帮助您在AI领域取得更大的成功。
+        </p>
 
+        <Button styles="mt-10" />
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index}/>
+        ))}
+      </div>
     </section>
   )
 }
